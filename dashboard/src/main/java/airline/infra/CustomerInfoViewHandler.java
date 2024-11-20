8225355460b2b1d96736d84f359b3d821daf10c1
hyerimmy <hyerim2000@swu.ac.kr>
2024-11-21 00:23:39 +0900

    chore: CustomerInfo CQRS 로직 수정

--- dashboard/src/main/java/airline/infra/CustomerInfoViewHandler.java
+++ dashboard/src/main/java/airline/infra/CustomerInfoViewHandler.java
@@ -11,6 +11,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class CustomerInfoViewHandler {
 
     //<<< DDD / CQRS
@@ -30,44 +31,16 @@ public class CustomerInfoViewHandler {
             );
 
             // 기존 customerId의 대시보드 데이터가 존재하지 않는다면 생성, 존재한다면 수정
-            if (customerInfoOptional.isPresent()) { 
+            if (customerInfoOptional.isPresent()) {  // 수정
                 CustomerInfo customerInfo = customerInfoOptional.get();
-                // view 객체에 이벤트의 eventDirectValue 를 set 함
-                customerInfo.setFlightCount(customerInfo.getFlightCount() + 1);
+                customerInfo.setFlightCount(customerInfo.getFlightCount()+1L);
                 customerInfo.setRecentReserveDate(reservationPlaced.getReserveDate());
-                // view 레파지 토리에 save
                 customerInfoRepository.save(customerInfo);
-            } else {
-                // view 객체 생성
+            } else { // 생성
                 CustomerInfo customerInfo = new CustomerInfo();
-                // view 객체에 이벤트의 Value 를 set 함
                 customerInfo.setCustomerId(reservationPlaced.getCustomerId());
                 customerInfo.setFlightCount(1L);
                 customerInfo.setRecentReserveDate(reservationPlaced.getReserveDate());
-                // view 레파지 토리에 save
-                customerInfoRepository.save(customerInfo);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @StreamListener(KafkaProcessor.INPUT)
-    public void whenReservationCanceled_then_UPDATE_2(
-        @Payload ReservationCanceled reservationCanceled
-    ) {
-        try {
-            if (!reservationCanceled.validate()) return;
-            // view 객체 조회
-            Optional<CustomerInfo> customerInfoOptional = customerInfoRepository.findById(
-                reservationCanceled.getCustomerId()
-            );
-
-            if (customerInfoOptional.isPresent()) {
-                CustomerInfo customerInfo = customerInfoOptional.get();
-                // view 객체에 이벤트의 eventDirectValue 를 set 함
-                customerInfo.setFlightCount(customerInfo.getFlightCount() - 1);
-                // view 레파지 토리에 save
                 customerInfoRepository.save(customerInfo);
             }
         } catch (Exception e) {
